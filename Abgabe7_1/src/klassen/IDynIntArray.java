693710e995b574474b5344f6d45ed8fb3defe770
felix <felix@felix-Lenovo-IdeaPad-Y500>
2018-06-07 23:05:05 +0200

    1a)

--- Abgabe7_1/src/klassen/IDynIntArray.java
+++ Abgabe7_1/src/klassen/IDynIntArray.java
@@ -1,6 +1,11 @@
 package klassen;
 
 public interface IDynIntArray {
-	
+	void add(int e);
+	void setElementAt(int i, int e); 
+	int getElementAt(int i);
+	int getElementCount();
+	void print();
+
 
 }
