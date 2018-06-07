614820ff991554e13f127278ac915df44b8aaf23
felix <felix@felix-Lenovo-IdeaPad-Y500>
2018-06-08 01:37:16 +0200

    abstract

--- Abgabe7_1/src/klassen/ADynIntArray.java
+++ Abgabe7_1/src/klassen/ADynIntArray.java
@@ -1,5 +1,9 @@
 package klassen;
 
 public abstract class ADynIntArray implements IDynIntArray{
-
+	public abstract void add(int e);
+	public abstract void setElementAt(int i, int e); 
+	public abstract int getElementAt(int i);
+	public abstract int getElementCount();
+	public abstract void print();
 }
