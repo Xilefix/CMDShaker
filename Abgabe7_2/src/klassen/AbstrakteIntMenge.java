7bfee56d56c94d25291fd0ef02e3ce6bcebbb9d7
felix <felix@felix-Lenovo-IdeaPad-Y500>
2018-06-08 02:39:38 +0200

    abstrakt

---
+++ Abgabe7_2/src/klassen/AbstrakteIntMenge.java
@@ -0,0 +1,53 @@
+package klassen;
+
+public abstract class AbstrakteIntMenge implements IntMenge {
+
+	@Override
+	public void add(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void remove(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean contains(int i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getSum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[] getElements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Abgabe7_2/src/klassen/IntMenge.java
@@ -0,0 +1,16 @@
+package klassen;
+
+public interface IntMenge {
+
+		public void add(int i);
+		public void remove(int i);
+		public boolean contains(int i);
+		public boolean isEmpty();
+		public int getSize();
+		public int getSum();
+		public int[] getElements();
+		public void print();
+	
+
+
+}
