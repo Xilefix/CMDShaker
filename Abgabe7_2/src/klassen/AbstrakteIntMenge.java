fa8ed49600295df05e2742bb0a2e233e6fb21dde
felix <felix@felix-Lenovo-IdeaPad-Y500>
2018-06-08 03:39:03 +0200

    dsaf

--- Abgabe7_2/src/klassen/AbstrakteIntMenge.java
+++ Abgabe7_2/src/klassen/AbstrakteIntMenge.java
@@ -3,44 +3,32 @@ package klassen;
 public abstract class AbstrakteIntMenge implements IntMenge {
 
 	@Override
-	public void add(int i) {
-
-
-	}
-
-	@Override
-	public void remove(int i) {
-
-	}
-
-	@Override
-	public boolean contains(int i) {
-		return false;
-	}
-
-	@Override
 	public boolean isEmpty() {
+		if(this.getSize()==0)
+		return true;
+        else 
 		return false;
 	}
 
-	@Override
-	public int getSize() {
-		return 0;
-	}
 
 	@Override
 	public int getSum() {
-		return 0;
-	}
-
-	@Override
-	public int[] getElements() {
-		return null;
+		int arr[];
+		arr	= getElements();
+		int sum = 0;
+		for(int counter=0;counter<getSize()-1;counter++){
+			sum+=arr[counter];
+    }
+    return sum;
 	}
 
 	@Override
 	public void print() {
-
+		int a[]= this.getElements();
+		for(int s : a)
+		{
+			System.out.println(s);
+		}
 	}
 
 }
