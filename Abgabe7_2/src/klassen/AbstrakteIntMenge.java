8edf24b3c15e04bf29a76321074094ba3502d30e
felix <felix@felix-Lenovo-IdeaPad-Y500>
2018-06-08 03:42:22 +0200

    b

--- Abgabe7_2/src/klassen/AbstrakteIntMenge.java
+++ Abgabe7_2/src/klassen/AbstrakteIntMenge.java
@@ -1,7 +1,11 @@
 package klassen;
 
 public abstract class AbstrakteIntMenge implements IntMenge {
-
+	public abstract void add(int i);
+    public abstract void remove(int i);
+    public abstract boolean contains(int i);
+    public abstract int getSize();
+    public abstract int[] getElements();
 	@Override
 	public boolean isEmpty() {
 		if(this.getSize()==0)
