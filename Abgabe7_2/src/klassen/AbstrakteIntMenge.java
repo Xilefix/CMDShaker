30357372bd2767b1890c50916e77d413598aef1f
felix <felix@felix-Lenovo-IdeaPad-Y500>
2018-06-08 02:46:19 +0200

    asd

--- Abgabe7_2/src/klassen/AbstrakteIntMenge.java
+++ Abgabe7_2/src/klassen/AbstrakteIntMenge.java
@@ -16,37 +16,31 @@ public abstract class AbstrakteIntMenge implements IntMenge {
 
 	@Override
 	public boolean contains(int i) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int getSum() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int[] getElements() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
 
 	}
 
