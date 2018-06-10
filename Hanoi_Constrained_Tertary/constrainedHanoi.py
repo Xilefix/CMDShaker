38761df7645a228e5130f1721bc81b38751f4f1e
felix <felix@felix-Lenovo-IdeaPad-Y500>
2018-06-10 21:57:26 +0200

    middle

--- Hanoi_Constrained_Tertary/constrainedHanoi.py
+++ Hanoi_Constrained_Tertary/constrainedHanoi.py
@@ -1,1 +1,2 @@
 print('"There are 10 types of people in this world; those who don\'t understand binary, those who do, and those that understand a base three joke."')
+
--- Hanoi_standard_binary/standardHanoi.py
+++ Hanoi_standard_binary/standardHanoi.py
@@ -1,2 +1,17 @@
 print('"There are 10 types of people in the world; Those who understand binary, and those who don\'t."')
 
+def Turm(k,a,b):
+    if k==0:
+        return
+    c = 6-a-b
+    #turm k-1 verschieben 
+    Turm(k-1, a, c)
+    print ("Bewege Disk",k, "von ",a,"nach",b)
+    print (k,a,b)
+    #Turm k-1 verschieben       
+    Turm(k-1, c, b)
+    
+        
+Turm(3, 1, 2)
+
+#https://sbj42.github.io/projects/tower-of-hanoi/dist/
