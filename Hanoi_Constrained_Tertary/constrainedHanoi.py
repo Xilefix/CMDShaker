181b219be0bb01230a6558e220c16d50670238b0
felix <felix@felix-Lenovo-IdeaPad-Y500>
2018-06-10 22:55:39 +0200

    binaer

--- Hanoi_Constrained_Tertary/constrainedHanoi.py
+++ Hanoi_Constrained_Tertary/constrainedHanoi.py
@@ -1,2 +1,4 @@
 print('"There are 10 types of people in this world; those who don\'t understand binary, those who do, and those that understand a base three joke."')
 
+def tertiar(zahl):
+    d
---
+++ Hanoi_standard_binary/binary.py
@@ -0,0 +1,24 @@
+#IntegerExponent[Range[2^8 - 1], 2] + 1
+def dual(zahl):
+    ganz = zahl // 2
+    rest = zahl % 2
+    if rest == 0:
+        zeichen = '0'
+    else:
+        zeichen = '1'
+    if ganz == 0:
+        return zeichen
+    else:
+        return dual(ganz) + zeichen
+    
+
+k=3    
+n = 1
+m=2**k
+for n in range(0,m):
+    print (dual(n))
+    
+#letztes Bit: Disk 1(kleinste)
+#vorletzte Bit: Disk 2
+#...
+#erste Bit: Disk k (groeste Disk)
