bea5d99025fc9e073a531b7f21938daf09df68c6
Felix <Felix@Felix.fritz.box>
2018-06-11 11:51:36 +0200

    finished

--- Kalender/Kalender.py
+++ Kalender/Kalender.py
@@ -1,4 +1,3 @@
-from calendar import February, January
 def schaltjahr(jahr):
     if jahr % 400 == 0 or jahr % 4 == 0 and jahr % 100 != 0:
         return True
--- Kalender/standard.py
+++ Kalender/standard.py
@@ -3,6 +3,6 @@ c = calendar.TextCalendar()
 m = 1
 y = input('year=')
 while m != 13:
-   str = c.formatmonth(y,m)
-   m = m + 1
-   print (str)
+    str = c.formatmonth(y,m)
+    m = m + 1
+    print (str)
