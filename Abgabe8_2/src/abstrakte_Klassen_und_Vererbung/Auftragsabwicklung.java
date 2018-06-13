8b2e2d8b1d0566b8d7916aaa7b66ec1fffc7a065
Felix <Felix@Felix.fritz.box>
2018-06-13 22:51:29 +0200

    main

---
+++ Abgabe8_2/src/abstrakte_Klassen_und_Vererbung/Auftragsabwicklung.java
@@ -0,0 +1,20 @@
+package abstrakte_Klassen_und_Vererbung;
+
+public class Auftragsabwicklung {
+	
+public static void main(String args[]) {
+	final double produktion = 200; // Wochenproduktion
+	// f¨ulle die Bestellung mit Fl¨ussigkeitsbeh¨altnissen
+	Behaeltnis[] bestellung = {
+			new Tetrapack(2.3, 0.9, 0.7), new Dose(0.59 , 1.15),
+			new Dose(0.59 , 1.15), new Tetrapack(1.1, 0.7, 0.7) // ...
+	};
+	// gebe Liste aller bestellten Fl¨ussigkeitsbeh¨altnisse aus
+	for (int i = 0; i < bestellung.length; i++) {
+		bestellung[i].println();
+	}
+	// berechne das Gesamtvolumen der Bestellung
+	// und die Restmenge der Wochenproduktion
+	// >>> Diesen Programmteil sollen Sie hier erg¨anzen! <<<
+}
+}
