11bc58380893a3b6e119d1a2a7128457fcbe8184
Felix <Felix@Felix.fritz.box>
2018-06-15 01:33:34 +0200

    einrueckung

--- Abgabe8_2/src/abstrakte_Klassen_und_Vererbung/Auftragsabwicklung.java
+++ Abgabe8_2/src/abstrakte_Klassen_und_Vererbung/Auftragsabwicklung.java
@@ -4,27 +4,27 @@ public class Auftragsabwicklung {
 	
 public static void main(String args[]) {
 	final double produktion = 200; // Wochenproduktion
-	// f¨ulle die Bestellung mit Fl¨ussigkeitsbeh¨altnissen
-	Behaeltnis[] bestellung = {
+		// f¨ulle die Bestellung mit Fl¨ussigkeitsbeh¨altnissen
+		Behaeltnis[] bestellung = {
 			new Tetrapack(2.3, 0.9, 0.7), new Dose(0.59 , 1.15),
 			new Dose(0.59 , 1.15), new Tetrapack(1.1, 0.7, 0.7) // ...
-	};
-	// gebe Liste aller bestellten Fl¨ussigkeitsbeh¨altnisse aus
-	for (int i = 0; i < bestellung.length; i++) {
-		bestellung[i].println();
+		};
+		// gebe Liste aller bestellten Fl¨ussigkeitsbeh¨altnisse aus
+		for (int i = 0; i < bestellung.length; i++) {
+			bestellung[i].println();
+		}
+		// berechne das Gesamtvolumen der Bestellung
+		double gesamtvolumen = 0.0;
+		for (int i = 0; i < bestellung.length; i++) {
+			gesamtvolumen = gesamtvolumen + bestellung[i].volumen();
+		}
+		System.out.println(gesamtvolumen);
+		// und die Restmenge der Wochenproduktion
+		double restmengewoche = produktion - gesamtvolumen;
+		if (produktion == restmengewoche)
+		{
+			System.out.println(restmengewoche);
+		}
+	
 	}
-	// berechne das Gesamtvolumen der Bestellung
-	double gesamtvolumen = 0.0;
-	for (int i = 0; i < bestellung.length; i++) {
-		gesamtvolumen = gesamtvolumen + bestellung[i].volumen();
-	}
-	System.out.println(gesamtvolumen);
-	// und die Restmenge der Wochenproduktion
-	double restmengewoche = produktion - gesamtvolumen;
-	if (produktion == restmengewoche)
-	{
-		System.out.println(restmengewoche);
-	}
-	// >>> Diesen Programmteil sollen Sie hier erg¨anzen! <<<
-}
 }
