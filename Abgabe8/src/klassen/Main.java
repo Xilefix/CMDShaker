7c2c80271efd339a5f4e45b480380c7b088493e1
Felix <Felix@Felix.fritz.box>
2018-06-15 02:10:42 +0200

    klammern

--- Abgabe8/src/klassen/Main.java
+++ Abgabe8/src/klassen/Main.java
@@ -5,13 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		Rectangle[] rects = new Rectangle[4];// siehe 2te Aufgabe array Erstellung
 		
-		{
+		
 			for (int i=0; i<4; i++)
 			{
 				rects[i] = new Rectangle(4,3,3,1);
 			}
 
-		}
+		
 
 	}
 
