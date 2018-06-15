f92779bfc910e694024cf000594644ef3859aa78
Felix <Felix@Felix.fritz.box>
2018-06-15 02:35:44 +0200

    main removed

--- Abgabe8/src/klassen/Rectangle.java
+++ Abgabe8/src/klassen/Rectangle.java
@@ -47,4 +47,21 @@ public class Rectangle implements ResizeableGO, MoveableGO {
 		this.width = width;
 	}
 
-}
+
+	public static void resizeAll(float r, ResizeableGO rgo[]) {
+		for (ResizeableGO g : rgo) g.resize(r);
+	}
+
+	public static void moveAll(float dx, float dy, MoveableGO mgo[]) {
+		for (MoveableGO m : mgo) m.move(dx, dy);
+	}
+
+public static void main(String[] args) {
+	Rectangle[] rects = new Rectangle[4];// siehe 2te Aufgabe array Erstellung
+	//Objecte in Array initialisieren 
+	rects[0] = new ReResizeableGO();
+	resizeAll(2.0, rects[4]);
+	moveAll(dx, dy, mgo);
+	
+
+
