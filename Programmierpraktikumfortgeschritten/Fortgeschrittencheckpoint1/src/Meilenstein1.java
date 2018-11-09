667fce938a999f825c0c388f70b77a7af9f9c009
wi34nid <wi34nid@ppc663.mirz.uni-jena.de>
2018-11-09 11:34:59 +0100

    rechtschreibung

--- Programmierpraktikumfortgeschritten/Fortgeschrittencheckpoint1/src/Meilenstein1.java
+++ Programmierpraktikumfortgeschritten/Fortgeschrittencheckpoint1/src/Meilenstein1.java
@@ -12,7 +12,7 @@ public class Meilenstein1 {
 
 	public static void main(String args[]) {
 		System.out.println("Geben Sie die Serveradresse POP3 ein:");
-		Scanner sc = new Scanner(System.in); // eingabe wird gescannt und in sc
+		Scanner sc = new Scanner(System.in); // Eingabe wird gescannt und in sc
 												// gespeichert
 		String url = sc.nextLine(); // einlesen der adresse POP3
 		System.out.println("Geben Sie am besten direkt den Port ein:");
