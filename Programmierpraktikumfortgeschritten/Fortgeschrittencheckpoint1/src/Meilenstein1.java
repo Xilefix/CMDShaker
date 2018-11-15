c4f2511aa35e8466d96a34ba64767035921db5d2
Felix <Felix@Felix>
2018-11-15 15:59:54 +0100

    finished

--- Programmierpraktikumfortgeschritten/Fortgeschrittencheckpoint1/src/Meilenstein1.java
+++ Programmierpraktikumfortgeschritten/Fortgeschrittencheckpoint1/src/Meilenstein1.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -15,9 +14,10 @@ public class Meilenstein1 {
 		Scanner sc = new Scanner(System.in); // eingabe wird gescannt und in sc
 												// gespeichert
 		String url = sc.nextLine(); // einlesen der adresse POP3
-		System.out.println("Geben Sie am besten direkt den Port ein:");
+		System.out.println("Geben Sie bitte jetzt den Port ein:");
 		int port = Integer.valueOf(sc.nextLine()); // string in Zahl umwandeln
-		System.out.println("Wir verbinden uns nun mit dem Server " + url + ":"+ port + ". Bitte warten. :)");
+		System.out.println("Wir verbinden uns nun mit dem Server. " + url + ":"
+				+ port + ". Bitte warten Sie solange.");
 		try {
 			Socket server = new Socket(url, port);
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
@@ -25,16 +25,17 @@ public class Meilenstein1 {
 			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
 					server.getOutputStream())); // schreiben nachricht an server
 
+			ArrayList<Email> email = new ArrayList<>();
 			String antwort = reader.readLine();
 			if (antwort.startsWith("+OK")) {
-				System.out.println("Geben Sie Ihren User Namen ein:");
+				System.out.println("Geben Sie Ihren Ihre Emailadresse ein:");
 				String user = sc.nextLine();
 				writer.write("USER " + user + "\n");
 				writer.flush();
 
 				antwort = reader.readLine();
 				if (antwort.startsWith("+OK")) {
-					System.out.println("Geben Sie Ihr Passwort ein:");
+					System.out.println("Geben Sie bitte Ihr Passwort ein:");
 					String pass = sc.nextLine();
 					writer.write("PASS " + pass + "\n");
 					writer.flush();
@@ -52,42 +53,81 @@ public class Meilenstein1 {
 							System.out.println("Sie haben " + anzahlEmails
 									+ " Email(s) erhalten.\n");
 
-							for (int i = 1; i <= anzahlEmails; i++) {		//Wir gehen alle emails im Postfach durch und lesen ein
+							for (int i = 1; i <= anzahlEmails; i++) { // Wir gehen alle Emails im Postfach zu und lesen ein							
 								writer.write("RETR " + i + "\n");
 								writer.flush();
 
-								ArrayList<String> zeilen = new ArrayList<>(); 	//wir erstellen liste
+								ArrayList<String> zeilen = new ArrayList<>(); // wir erstellen liste
 
-								while (true) {						//Email zeilenweise durchlesen bis zum  Punkt, dann abrechnen
-									antwort = reader.readLine();	//speichern zeilen in liste
+								while (true) { // Email zeilenweise durchlesen bis Punkt, dann abbrechen
+									antwort = reader.readLine(); // speichern zeilen in liste
 									if (antwort.equals("."))
 										break;
 									zeilen.add(antwort);
 								}
+								email.add(new Email(i, zeilen));
+								// Email email = new Email(i, zeilen);
 
-								Email email = new Email(i, zeilen);	//speichern Liste an Zeilen in neue Email
+							}
+							System.out
+									.println("Ihre Email(s) sind fertig geladen.\n");
+							while (true) { // optionentafel erstellen
+								System.out.println("Das sind Ihre Optionen: ");
+								System.out.println("-1 : Beenden.");
+								System.out.println("0 : empfangene Emails als Liste anzeigen.");
+								System.out.println("n : eine bestimmte (n-te) Email anzeigen.");
 
+								int eingabe = Integer.valueOf(sc.nextLine());
+								if (eingabe == -1) {
+									break;
+								} else if (eingabe == 0) {
+									System.out.println("Hier finden Sie Ihre Email(s):");
+									for (int i = 1; i <= anzahlEmails; i++) { // Emails durchgehen und auflisten
+										Email mail = email.get(i - 1);
+										System.out.println(mail.nummer + ": " + mail.from + " " + mail.subject);
+									}
+									System.out.println("");
+								} else if (eingabe > 0) {
+									System.out.println("\nDas ist die " + eingabe + ". Email:");
+									Email mail = email.get(eingabe - 1);
+									for (int i = 0; i < mail.zeilen.size(); i++) {
+										System.out.println(mail.zeilen.get(i));
+									}
+								}
 							}
 
 						} else {
-							System.out.println("Wir haben einen Fehler beim Einlesen der Emails.");
+							System.out
+									.println("Wir haben einen Fehler beim Einlesen der Emails.");
 						}
 
 					} else {
-						System.out.println("Wir haben ein Problem mit Ihrem Benutzernamen oder Ihrem Passwort. Ändern Sie das Bitte!");
+						System.out
+								.println("Wir haben ein Problem mit Ihrem Benutzernamen oder Ihrem Passwort. Ändern Sie das Bitte!");
 					}
 
 				} else {
-					System.out.println("Wir haben ein Problem mit Ihrem Benutzernamen. Ändern Sie das Bitte!");
+					System.out
+							.println("Wir haben ein Problem mit Ihrem Benutzernamen. Ändern Sie das Bitte!");
 				}
 			} else {
-				System.out.println("Die Verbindung ist fehlgeschlagen. Das tut uns Leid.");
+				System.out
+						.println("Die Verbindung ist fehlgeschlagen. Das tut uns Leid.");
 			}
+			
+			writer.write("QUIT\n");
+			writer.flush();
+			antwort = reader.readLine();
+			if (antwort.startsWith("+OK")){
+				System.out.println("Das Programm wird beendet. Bis demnächst!");
+			}
+			
 			server.close();
-		} catch (IOException e) { // Fehler fangen, falls falsche Eingabe oder
-									// Server unereichbar
+			
+		} catch (IOException e) { // Fehler fangen, falls falsche Eingabe oder server unerreichbar
 			e.printStackTrace();
-			System.out.println("Sorry, Sie haben sich da wohl vertippt oder der Server schläft. Bitte versuchen Sie es erneut:");
+			System.out
+					.println("Sorry, Sie haben sich da wohl vertippt oder der Server schläft. Bitte versuchen Sie es erneut:");
 		}
 
 		sc.close(); // Scanner schließen
@@ -106,13 +146,11 @@ class Email {
 		for (int i = 0; i < zeilen.size(); i++) {
 			String zeile = zeilen.get(i);
 			if (zeile.startsWith("Subject: ")) {
-				subject = zeile.substring(9);
+				subject = zeile.substring(9, zeile.length());
 			}
 			if (zeile.startsWith("From: ")) {
-				from = zeile.substring(6);
+				from = zeile.substring(6, zeile.length());
 			}
 		}
 	}
 }
-
-
