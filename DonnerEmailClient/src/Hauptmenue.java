e24ccf318a62b7b70b512720a774e11a86e1ed22
Felix <Felix@Felix.fritz.box>
2019-01-26 23:34:44 +0100

    4datei

--- DonnerEmailClient/src/Hauptmenue.java
+++ DonnerEmailClient/src/Hauptmenue.java
@@ -21,10 +21,10 @@ import javax.net.ssl.*;
 import java.util.Properties;
 
 public class Hauptmenue{
-	public static String sendServer;
-	public static String recServer;
-	public static String name;
-	public static String passwort;
+	//public static String sendServer;
+	//public static String recServer;
+	//public static String name;
+	//public static String passwort;
 	
 	public static void main(String[] args) {
 		
